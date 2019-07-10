81c90a65cf435874e5eeb80eb07beda1a49aec46
Nathan Tu <nathantuey@gmail.com>
2019-07-10 13:56:35 +1000

    Setting up Basic Navigation
    Added Routes for Login, registration, Dashboard, Posts, Forum, Landing page

---
+++ src/components/forum/Posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to FoodLadder Forum</h1>
+      </div>
+    );
+  }
+}
+
+export default Posts;
