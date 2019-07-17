e6913f410e678e71a463c83de2f66dc7decad729
Clementine <clementine.oldfield@gmail.com>
2019-07-17 22:33:41 +1000

    Configured Thread replies
    And storing current thread in redux state in order to set from the NewPost thread.

--- src/components/forum/thread/ThreadPage.js
+++ src/components/forum/thread/ThreadPage.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
+import { Row, Col } from "antd";
+import { connect } from "react-redux";
 
 import LocalAPI from "./../../../apis/local";
 import Post from "./post/Post";
 import { FullPage } from "./../../layout/Layout";
+import NewPostForm from "./../../forms/NewPostForm";
+import { setThread } from "./../../../actions";
 
 class ThreadPage extends Component {
-
-  state = {
-    thread: null
-  }
 
   componentDidMount = () => {
     const { id } = this.props.match.params;
     LocalAPI.get(`/threads/${id}`)
       .then(res => {
-        this.setState({thread: res.data});
+        console.log(res.data);
+        this.props.setThread(res.data);
       }).catch( err => {
         console.log(err);
       });
   }
 
   render() {
-    const { thread } = this.state;
+    const { thread } = this.props;
     return (
       <FullPage>
-        { thread && thread.posts.map(post => {
-          return <Post key={post._id}>{post.body}</Post>
-        })}
+        <Row>
+          <Col span={6}></Col>
+          <Col span={12}>
+            { thread && thread.posts.map(post => {
+              return <Post key={post._id}>{post.body}</Post>
+            })}
+            { thread && 
+              <NewPostForm thread={thread} />
+            }
+          </Col>
+          <Col span={6}></Col>
+        </Row>
       </FullPage>
     );
   }
 }
 
-export default ThreadPage;
+const mapStateToProps = (state) => {
+  return {
+    thread: state.forum.thread,
+  }
+}
+
+export default connect(mapStateToProps, { setThread })(ThreadPage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import { reducer as formReducer } from "redux-form";
 import headerStylesReducer from "./header_styles";
 import authReducer from "./auth_reducer";
 import userReducer from "./user_reducer";
+import threadReducer from "./thread_reducer";
 
 export default combineReducers({
   form: formReducer,
   headerStyles: headerStylesReducer,
   auth: authReducer,
-  user: userReducer
+  user: userReducer,
+  forum: threadReducer
 });
