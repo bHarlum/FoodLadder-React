79900810d1f3ee46831517af2b1838c9901b694f
Clementine <clementine.oldfield@gmail.com>
2019-07-18 11:30:22 +1000

    Updated styling on forum page.
    Ant List. Needs finishing.

--- src/components/layout/header/HeaderStyles.js
+++ src/components/layout/header/HeaderStyles.js
@@ -4,29 +4,29 @@ import { Dropdown as AntDropdown } from "antd";
 import antStyled from "./../../antStyled";
 
 export const HeaderContainer = styled.div`
-position: ${ props => props.position || "static" };
-width: 100%;
-display: flex;
-justify-content: space-around;
-align-items: center;
+  position: ${ props => props.position || "static" };
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const Float = styled.div`
-position: absolute;
-right: 40px;
-top: 45px;
+  position: absolute;
+  right: 40px;
+  top: 45px;
 `
 
 export const UserBadge = styled.div`
-width: 250px;
-padding: 15px 30px;
+  width: 250px;
+  padding: 15px 30px;
 `;
 
 export const Name = styled.h3`
-display: inline;
-margin: 0 0 0 15px;
+  display: inline;
+  margin: 0 0 0 15px;
 `;
 
 export const Dropdown = antStyled(AntDropdown)`
-cursor: pointer;
+  cursor: pointer;
 `;
