2d08bcdfaefb0db71031b854b419ef2bae5eca24
Clementine <clementine.oldfield@gmail.com>
2019-07-18 22:03:24 +1000

    Added list of all existing projects on admin dashboard

--- src/components/dashboard/AdminDashboard.js
+++ src/components/dashboard/AdminDashboard.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
-import { Card, Button } from "antd";
+import { Card, Button, List } from "antd";
 import { Link } from "react-router-dom";
+
+import LocalAPI from "./../../apis/local";
 
 class AdminDashboard extends Component {
 
-  render(){
+  state = {
+    projects: []
+  }
 
+  componentDidMount() {
+    LocalAPI.get("/projects/")
+      .then( response => {
+        this.setState({projects: response.data})
+      }).catch( err => {
+        console.log(err);
+      })
+  }
+
+  render(){
+    const { projects } = this.state;
     return(
       <Card>
+        <List
+          dataSource={projects}
+          renderItem={item => {
+            return(
+              <List.Item>
+                <List.Item.Meta 
+                  title={item.name}
+                />
+              </List.Item>
+            );
+          }}
+        />
         <Link to="/projects/new">
           <Button type="primary">Add a new Project</Button>
         </Link>
