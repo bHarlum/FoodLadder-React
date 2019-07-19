a62a8da1f5406d95071ed795d451ed69e8a22503
Nathan Tu <nathantuey@gmail.com>
2019-07-19 22:18:20 +1000

    Updated Links in App for Faq and Privacy

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Layout, Spin } from "antd";
 import { connect } from "react-redux";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 import PrivateRoute from "./PrivateRoute";
 import UnregisteredRoute from "./UnregisteredRoute";
@@ -32,26 +32,48 @@ class App extends Component {
     let { loading } = this.props;
     return (
       <div>
-        { loading &&
+        {loading && (
           <Centered overlay>
             <Spin size="large" />
           </Centered>
-        }
+        )}
         <BrowserRouter>
           <Layout>
             <Header />
             <Content>
               <Switch>
-                <UnregisteredRoute exact path='/' component={Landing} />
-                <Route exact path='/register/:id' component={Register} />
-                <UnregisteredRoute exact path='/login' component={Login} />
-                <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                <PrivateRoute exact path='/forum' component={Forum} />
-                <PrivateRoute exact path="/forum/threads/new" component={NewThread} />
-                <PrivateRoute exact path="/forum/threads/:id" component={ThreadPage} />
-                <PrivateRoute exact path='/projects' component={Project} />
-                <PrivateRoute exact path='/projects/new' component={NewProject} />
-                <UnregisteredRoute exact path='/loginform' component={LoginForm} />
+                <UnregisteredRoute exact path="/" component={Landing} />
+                <Route exact path="/register/:id" component={Register} />
+                <UnregisteredRoute exact path="/login" component={Login} />
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/forum" component={Forum} />
+                <PrivateRoute
+                  exact
+                  path="/forum/threads/new"
+                  component={NewThread}
+                />
+                <PrivateRoute
+                  exact
+                  path="/forum/threads/:id"
+                  component={ThreadPage}
+                />
+                <PrivateRoute exact path="/projects" component={Project} />
+                <PrivateRoute
+                  exact
+                  path="/projects/new"
+                  component={NewProject}
+                />
+                <UnregisteredRoute
+                  exact
+                  path="/loginform"
+                  component={LoginForm}
+                />
+                <UnregisteredRoute exact path="/forum/faq" component={Faq} />
+                <UnregisteredRoute
+                  exact
+                  path="/forum/privacypolicy"
+                  component={PrivacyPolicy}
+                />
               </Switch>
             </Content>
             <AntFooter>
@@ -64,10 +86,13 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     loading: state.loading.spinner
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, {})(App);
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
