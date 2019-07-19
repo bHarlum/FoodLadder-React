d5149c2449058fa5e2dc6d8101664f6434fb609f
Nathan Tu <nathantuey@gmail.com>
2019-07-19 12:43:12 +1000

    Added links for Social media, Faq, Privacy and Contact to Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
 import { Layout } from "antd";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 import PrivateRoute from "./PrivateRoute";
 import UnregisteredRoute from "./UnregisteredRoute";
 
-import Landing from './Landing';
-import Register from './auth/Register';
-import Login from './auth/Login';
-import Forum from './forum/Forum';
-import Project from './projects/Project';
-import NewProject from './projects/NewProject';
-import Dashboard from './dashboard/Dashboard';
+import Landing from "./Landing";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Forum from "./forum/Forum";
+import Project from "./projects/Project";
+import NewProject from "./projects/NewProject";
+import Dashboard from "./dashboard/Dashboard";
 import ThreadPage from "./forum/thread/ThreadPage";
 import NewThread from "./forum/thread/NewThread";
 import Faq from "./forum/Faq";
 import PrivacyPolicy from "./forum/PrivacyPolicy";
 
 import Header from "./layout/header/Header";
-import Footer from "./layout/Footer";
+import Footer from "./layout/footer/Footer";
 
 import LoginForm from "./forms/LoginForm";
 
@@ -34,22 +34,46 @@ class App extends Component {
             <Header />
             <Content>
               <Switch>
-                <UnregisteredRoute exact path='/' component={Landing} />
-                <UnregisteredRoute exact path='/register' component={Register} />
-                <UnregisteredRoute exact path='/login' component={Login} />
-                <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                <PrivateRoute exact path='/forum' component={Forum} />
-                <PrivateRoute exact path="/forum/threads/new" component={NewThread} />
-                <PrivateRoute exact path="/forum/threads/:id" component={ThreadPage} />
-                <PrivateRoute exact path='/projects' component={Project} />
-                <PrivateRoute exact path='/projects/new' component={NewProject} />
-                <UnregisteredRoute exact path='/loginform' component={LoginForm} />
-                <UnregisteredRoute exact path='/forum/faq' component={Faq} />
-                <UnregisteredRoute exact path='/forum/privacypolicy' component={PrivacyPolicy} />
+                <UnregisteredRoute exact path="/" component={Landing} />
+                <UnregisteredRoute
+                  exact
+                  path="/register"
+                  component={Register}
+                />
+                <UnregisteredRoute exact path="/login" component={Login} />
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/forum" component={Forum} />
+                <PrivateRoute
+                  exact
+                  path="/forum/threads/new"
+                  component={NewThread}
+                />
+                <PrivateRoute
+                  exact
+                  path="/forum/threads/:id"
+                  component={ThreadPage}
+                />
+                <PrivateRoute exact path="/projects" component={Project} />
+                <PrivateRoute
+                  exact
+                  path="/projects/new"
+                  component={NewProject}
+                />
+                <UnregisteredRoute
+                  exact
+                  path="/loginform"
+                  component={LoginForm}
+                />
+                <UnregisteredRoute exact path="/forum/faq" component={Faq} />
+                <UnregisteredRoute
+                  exact
+                  path="/forum/privacypolicy"
+                  component={PrivacyPolicy}
+                />
               </Switch>
             </Content>
             <AntFooter>
-              <Footer/>
+              <Footer />
             </AntFooter>
           </Layout>
         </BrowserRouter>
