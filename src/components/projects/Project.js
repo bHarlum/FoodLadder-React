57703b52e2f3e79178882bb3afb67142781cf3df
Bryce Harlum <bryce.harlum@gmail.com>
2019-07-22 11:10:55 +1000

    Updated project page
    Project basic profile setup 
    Bio, Picture, and Address.

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { statement } from '@babel/template';
-import { Typography } from "antd";
+import { Typography, Card } from "antd";
 import { connect } from "react-redux";
 
 import LocalAPI from "./../../apis/local";
 import { Section, FullPage } from './../layout/Layout';
 import { setSpinner } from "./../../actions/index";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export class Project extends Component {
 
   state = {
     project: null
   }
+  
 
   componentDidMount() {
     this.props.setSpinner(true);
@@ -32,12 +33,25 @@ export class Project extends Component {
 
   render() {
     const { project } = this.state;
+    console.log(project);
     return (
       <FullPage>
         <Section>
+          
           {project &&
-            <Title>{project.name}</Title>
+            <div>
+              <Card style={ {backgroundImage: 'url(https://foodladder.org/wp-content/uploads/2018/08/IMG_4397-1-2000x1200.jpg)'} }><Title>{project.name}</Title></Card>
+              <Card>
+                <Text>{`${project.address.line1}, ${project.address.line2}, ${project.address.city}, ${project.address.state}, ${project.address.country}.`}</Text>
+              </Card>
+              <Card>
+                <Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia erat eros, quis maximus neque feugiat pharetra. Donec mollis a magna vel pretium. Donec lacinia magna id odio ornare sagittis. Maecenas suscipit tellus sit amet viverra rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;.
+                </Text>
+              </Card>
+            </div>
           }
+          
         </Section>
       </FullPage>
     );
