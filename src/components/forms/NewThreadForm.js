75d957c92072249fb22a89cf54a8a88304c33cc5
Bryce Harlum <bryce.harlum@gmail.com>
2019-07-23 10:56:28 +1000

    started on thread upload form
    basic implementation

--- src/components/forms/NewThreadForm.js
+++ src/components/forms/NewThreadForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
-import { Button, Typography } from "antd";
+import { Button, Typography, message, Upload, Icon } from "antd";
 import { connect } from "react-redux";
 
 import LocalAPI from "./../../apis/local";
@@ -10,9 +10,36 @@ const { Title } = Typography;
 
 class NewThreadForm extends Component {
 
+  fileData = {
+    link: null,
+    size: null
+  }
+
+  uploadSettings = {
+    name: 'file',
+    action: `${process.env.REACT_APP_API_URL}/threads/upload`,
+    headers: {
+      authorization: `Bearer ${this.props.token}`
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        this.fileData.link = info.file.response.link;
+        this.fileData.size = info.file.response.size;
+
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+
   onFormSubmit = async (values) => {
     const { title, body } = values;
-    const data = { newThread: { title, posts: [] }};
+    const data = { newThread: { title, posts: [], file: this.fileData }};
     data.newThread.posts.push({
       body,
       author: this.props.currentUser
@@ -51,6 +78,12 @@ class NewThreadForm extends Component {
             Submit
           </Button>
         </form>
+        <Upload name="file" {...this.uploadSettings}>
+          <Button>
+            <Icon type="upload" /> 
+              Add an File
+          </Button>
+        </Upload>
       </FormContainer>
     );
   }
@@ -72,7 +105,8 @@ const validate = (formValues) => {
 
 const mapStateToProps = (state) => {
   return {
-    currentUser: state.user.current
+    currentUser: state.user.current,
+    token: state.auth.token
   }
 }
 
